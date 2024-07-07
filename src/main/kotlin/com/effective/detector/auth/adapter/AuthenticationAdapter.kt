879d200e9db39e6b73739605f2b01b6e5316b4ff
shkisme <shkisme0130@gmail.com>
2024-07-07 23:37:37 +0900

    style: 오타 수정

--- src/main/kotlin/com/effective/detector/auth/adapter/AuthenticationAdapter.kt
+++ src/main/kotlin/com/effective/detector/auth/adapter/AuthenticationAdapter.kt
@@ -3,7 +3,7 @@ package com.effective.detector.auth.adapter
 import com.effective.detector.member.domain.MemberRole
 
 /**
- * JWT를 만들기 위해 필요한 uuid와 memberRole을 반환하는 인터페이스를 정의
+ * JWT를 만들기 위해 필요한 id와 memberRole을 반환하는 인터페이스를 정의
  */
 interface AuthenticationAdapter {
 
