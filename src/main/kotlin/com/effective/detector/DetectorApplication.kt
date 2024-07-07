2332263db8e3d2ab3e991542182bbf0a2487aa23
shkisme <shkisme0130@gmail.com>
2024-07-07 20:19:36 +0900

    fix: 기본 로그인 제거

--- src/main/kotlin/com/effective/detector/DetectorApplication.kt
+++ src/main/kotlin/com/effective/detector/DetectorApplication.kt
@@ -1,9 +1,10 @@
 package com.effective.detector
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration
 import org.springframework.boot.runApplication
 
-@SpringBootApplication
+@SpringBootApplication(exclude = [SecurityAutoConfiguration::class])
 class DetectorApplication
 
 fun main(args: Array<String>) {
