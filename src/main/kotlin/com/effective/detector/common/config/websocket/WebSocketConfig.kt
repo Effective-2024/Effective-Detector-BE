d22ccdc6e2ce894bbfafeb478f60ed2b0ef1647f
shkisme <shkisme0130@gmail.com>
2024-08-18 15:13:56 +0900

    feat: 스트리밍 (1차 구현)

--- src/main/kotlin/com/effective/detector/common/config/websocket/WebSocketConfig.kt
+++ src/main/kotlin/com/effective/detector/common/config/websocket/WebSocketConfig.kt
@@ -1,6 +1,8 @@
 package com.effective.detector.common.config.websocket
 
 import org.springframework.context.annotation.Configuration
+import org.springframework.messaging.converter.MappingJackson2MessageConverter
+import org.springframework.messaging.converter.MessageConverter
 import org.springframework.messaging.simp.config.MessageBrokerRegistry
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry
@@ -11,13 +13,17 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 class WebSocketConfig : WebSocketMessageBrokerConfigurer {
 
     override fun configureMessageBroker(config: MessageBrokerRegistry) {
-        config.enableSimpleBroker("/topic") // 클라이언트가 구독할 수 있는 목적지(prefix)를 설정합니다.
+        config.enableSimpleBroker("/hospitals") // 클라이언트가 구독할 수 있는 목적지(prefix)를 설정합니다.
         config.setApplicationDestinationPrefixes("/app") // 메시지를 보낼 때 사용하는 prefix를 설정합니다.
     }
 
     override fun registerStompEndpoints(registry: StompEndpointRegistry) {
-        registry.addEndpoint("/image-stream")
+        registry.addEndpoint("/ws")
             .setAllowedOriginPatterns("*")
-            .withSockJS() // STOMP 엔드포인트를 설정하고, SockJS 지원을 추가합니다.
+    }
+
+    override fun configureMessageConverters(messageConverters: MutableList<MessageConverter>): Boolean {
+        messageConverters.add(MappingJackson2MessageConverter())
+        return false
     }
 }
