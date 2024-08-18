e193c9686ff3ad213bd77de393839313bbc837d4
shkisme <shkisme0130@gmail.com>
2024-08-18 15:46:54 +0900

    feat: CORS 에러 해결

---
+++ src/main/kotlin/com/effective/detector/common/config/CorsConfig.kt
@@ -0,0 +1,31 @@
+package com.effective.detector.common.config
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.cors.CorsConfiguration
+import org.springframework.web.cors.CorsConfigurationSource
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource
+import org.springframework.web.filter.CorsFilter
+
+@Configuration
+class CorsConfig {
+
+    @Bean
+    fun corsConfigurationSource(): CorsConfigurationSource {
+        val configuration = CorsConfiguration()
+        configuration.allowedOrigins = listOf("http://localhost:3000") // 허용할 도메인
+        configuration.allowedMethods = listOf("GET", "POST", "PUT", "DELETE", "PATCH")
+        configuration.allowedHeaders = listOf("Authorization", "Cache-Control", "Content-Type")
+        configuration.allowCredentials = true
+        configuration.maxAge = 3600
+
+        val source = UrlBasedCorsConfigurationSource()
+        source.registerCorsConfiguration("/**", configuration)
+        return source
+    }
+
+    @Bean
+    fun corsFilter(): CorsFilter {
+        return CorsFilter(corsConfigurationSource())
+    }
+}
