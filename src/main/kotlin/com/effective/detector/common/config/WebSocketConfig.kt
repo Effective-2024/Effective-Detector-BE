9944e96504701c7f5f150f640d8ba7e6eccad903
shkisme <shkisme0130@gmail.com>
2024-09-27 18:09:01 +0900

    feat: 이미지 및 사고 발생 여부 웹소켓 핸들러 작성

--- src/main/kotlin/com/effective/detector/common/config/WebSocketConfig.kt
+++ src/main/kotlin/com/effective/detector/common/config/WebSocketConfig.kt
@@ -1,23 +1,19 @@
 package com.effective.detector.common.config
 
+import com.effective.detector.common.handler.WebSocketAccidentDetectHandler
+import com.effective.detector.common.handler.WebSocketImageStreamHandler
 import org.springframework.context.annotation.Configuration
-import org.springframework.messaging.simp.config.MessageBrokerRegistry
-import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker
-import org.springframework.web.socket.config.annotation.StompEndpointRegistry
-import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer
+import org.springframework.web.socket.config.annotation.EnableWebSocket
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry
 
+
+@EnableWebSocket
 @Configuration
-@EnableWebSocketMessageBroker
-class WebSocketConfig : WebSocketMessageBrokerConfigurer {
+class WebSocketConfig : WebSocketConfigurer {
 
-    override fun configureMessageBroker(config: MessageBrokerRegistry) {
-        config.enableSimpleBroker("/topic") // 클라이언트가 구독할 수 있는 목적지(prefix)를 설정합니다.
-        config.setApplicationDestinationPrefixes("/app") // 메시지를 보낼 때 사용하는 prefix를 설정합니다.
-    }
-
-    override fun registerStompEndpoints(registry: StompEndpointRegistry) {
-        registry.addEndpoint("/image-stream")
-            .setAllowedOriginPatterns("*")
-            .withSockJS() // STOMP 엔드포인트를 설정하고, SockJS 지원을 추가합니다.
+    override fun registerWebSocketHandlers(registry: WebSocketHandlerRegistry) {
+        registry.addHandler(WebSocketImageStreamHandler(), "/image-stream").setAllowedOrigins("*")
+        registry.addHandler(WebSocketAccidentDetectHandler(), "/accident-detect").setAllowedOrigins("*")
     }
 }
